779c6ce29cb92f2af292ba8faaefcae8eca95c72
jkworks <info@jilliankayworks.com>
2021-07-17 09:32:56 -0600

    created variables have html and css mocked

---
+++ assets/javascript/javascript/script.js
@@ -0,0 +1,58 @@
+var timeEL = document.querySelector(".time")
+
+const question = document.querySelector(".questions")
+const answers = [.answer]
+const score = document.querySelector(".score")
+
+let questionEl = []
+let correctAnswers = true 
+let score = 0
+let questionCounter = 0 
+let availableQuestions = []
+
+
+let questions = {
+    
+}
+
+
+
+//create a quiz with buttons 
+//
+//variables 
+    //you will need ids and query selectors 
+
+//functions 
+    //create a timer that looses time when an answer is wrong
+    //deduct 10 seconds if you get an answer wrong 
+
+    //event listeners
+
+    // need questions need to find the correct answer 
+    //store all the questions and answers in an array 
+    // WILL NEED AN ARRAY OF OBJECTS 
+    //available array 
+    //each of your questions would be an object 
+    //object would have 3 properties 
+    //strign thats the question 
+    ///aray of answer list
+    //array of the correct answer 
+
+    //function just has one task update display all based of of the click 
+    
+
+    //how to automatically refresh you 
+    // you would want to use inner html and tak etags create a string that's a div
+    //that has a paragraph that the questions and a list with the answers send 
+    //textContent to replace the html 
+
+
+
+    //need data attribute for display 
+
+
+
+//will need event listeners 
+
+//if else statements && for correct answers 
+
